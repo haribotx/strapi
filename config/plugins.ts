5e28546ee658a39cbfd776cfe4a6dbbfeba51d2a
fayiz-123 <muhammedfayiz59@gmail.com>
2025-10-30 16:05:50 +0530

    plugin cofiguration chnaged for email setup in cloud.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,19 +1,31 @@
 export default ({ env }) => ({
   email: {
     config: {
-      provider: "nodemailer",
-      providerOptions: {
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-          user: env("SMTP_USERNAME"),
-          pass: env("SMTP_PASSWORD"),
-        },
-      },
+      provider:
+        env("NODE_ENV") === "production"
+          ? "strapi-provider-email-strapi-cloud"
+          : "nodemailer",
+      providerOptions:
+        env("NODE_ENV") === "production"
+          ? {}
+          : {
+              host: "smtp.gmail.com",
+              port: 465,
+              secure: true,
+              auth: {
+                user: env("SMTP_USERNAME"),
+                pass: env("SMTP_PASSWORD"),
+              },
+            },
       settings: {
-        defaultFrom: env("SMTP_USERNAME"),
-        defaultReplyTo: env("SMTP_USERNAME"),
+        defaultFrom:
+          env("NODE_ENV") === "production"
+            ? env("STRAPI_EMAIL_DEFAULT_FROM", "ihmsservice@gmail.com")
+            : env("SMTP_USERNAME"),
+        defaultReplyTo:
+          env("NODE_ENV") === "production"
+            ? env("STRAPI_EMAIL_DEFAULT_REPLY_TO", "ihmsservice@gmail.com")
+            : env("SMTP_USERNAME"),
       },
     },
   },
