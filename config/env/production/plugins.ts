35a3cbc8dadc8de87c3d9a9f8416848016c66e9f
fayiz-123 <muhammedfayiz59@gmail.com>
2025-11-04 12:28:36 +0530

    - typescript correction

--- config/env/production/plugins.ts
+++ config/env/production/plugins.ts
@@ -1,11 +1,11 @@
-module.exports = ({ env }) => ({
+export default ({ env }) => ({
   email: {
     config: {
-      provider: "strapi-provider-email-strapi-cloud",
+      provider: 'strapi-provider-email-strapi-cloud',
       providerOptions: {},
       settings: {
-        defaultFrom: env("STRAPI_EMAIL_DEFAULT_FROM", "ihmsservice@gmail.com"),
-        defaultReplyTo: env("STRAPI_EMAIL_DEFAULT_REPLY_TO", "ihmsservice@gmail.com"),
+        defaultFrom: env('STRAPI_EMAIL_DEFAULT_FROM', 'ihmsservice@gmail.com'),
+        defaultReplyTo: env('STRAPI_EMAIL_DEFAULT_REPLY_TO', 'ihmsservice@gmail.com'),
       },
     },
   },
