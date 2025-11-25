5e1587960ea8432624283838b5cceb211d41587d
libinkallingal <libinbotxBOTXSWe2403@gmail.com>
2025-11-25 10:09:34 +0530

    changes

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -542,6 +542,7 @@ export interface ApiProjectProject extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    icon: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
